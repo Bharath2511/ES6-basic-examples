db6b2adcc157c66a86f41fbf7b87beb596589376
V S Bharath <bharathcse5t6@gmail.com>
2018-10-03 22:26:43 +0530

    adding default parameters

--- es6.js
+++ es6.js
@@ -109,3 +109,13 @@ const squareList = (arr) => {
 // test your code
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
+
+//adding default parameters to a function
+const increment = (function() {
+    "use strict";
+    return function increment(number, value=1) {
+      return number + value;
+    };
+  })();
+  console.log(increment(5, 2)); // returns 7
+  console.log(increment(5)); // returns 6
