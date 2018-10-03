2f3f997489b82416939f6c930d8daaa239dcb995
V S Bharath <bharathcse5t6@gmail.com>
2018-10-03 20:26:08 +0530

    passing arguments into an arrow function

--- es6.js
+++ es6.js
@@ -87,3 +87,11 @@ function freezeObj() {
   }
   ;
   magic()
+
+//passing arguments into an arrow function
+  const myConcat = (arr1, arr2) => {
+    "use strict";
+    return arr1.concat(arr2);
+  };
+  // test your code
+  console.log(myConcat([1, 2], [3, 4, 5]));
